a8c64733ffbe1f4120dc42c29bdbac204db22a99
chetan pawar <chetan882777@gmail.com>
2020-10-07 22:00:15 +0530

    reformat code with darfmt constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,6 @@
-
-
 class Constants {
-	static const METHOD_CHANNEL_ID = "com.chetan.make_app_endless_plugin.method_channel";
-	static const METHOD_CALL_START_SERVICE = "method_call_start_service";
-	static const METHOD_CALL_STOP_SERVICE = "method_call_stop_service";
- }
+  static const METHOD_CHANNEL_ID =
+      "com.chetan.make_app_endless_plugin.method_channel";
+  static const METHOD_CALL_START_SERVICE = "method_call_start_service";
+  static const METHOD_CALL_STOP_SERVICE = "method_call_stop_service";
+}
