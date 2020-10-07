96677d02a0bb1078d1a705f3f5e2b30df54d5598
chetan pawar <chetan882777@gmail.com>
2020-10-07 13:00:48 +0530

    save activity life

---
+++ android/src/main/java/com/chetan/make_app_endless_plugin/ConstantsOnlyForAndroid.java
@@ -0,0 +1,9 @@
+package com.chetan.make_app_endless_plugin;
+
+public class ConstantsOnlyForAndroid {
+    public final static String PREF_NAME = "throw_prefs";
+
+    public final static String PREF_IS_THROW_ACTIVITY_ACTIVE = "pref_is_throw_activity_active";
+    public final static boolean PREF_THROW_ACTIVITY_ACTIVE = true;
+    public final static boolean PREF_IS_NOT_THROW_ACTIVITY_ACTIVE = false;
+}
--- android/src/main/java/com/chetan/make_app_endless_plugin/EndlessActivity.java
+++ android/src/main/java/com/chetan/make_app_endless_plugin/EndlessActivity.java
@@ -1,7 +1,29 @@
 package com.chetan.make_app_endless_plugin;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
 import io.flutter.app.FlutterActivity;
 
 public class EndlessActivity extends FlutterActivity {
 
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        preferences = getSharedPreferences(ConstantsOnlyForAndroid.PREF_NAME, MODE_PRIVATE);
+        preferences.edit().putBoolean(ConstantsOnlyForAndroid.PREF_IS_THROW_ACTIVITY_ACTIVE,
+                ConstantsOnlyForAndroid.PREF_THROW_ACTIVITY_ACTIVE).apply();
+    }
+
+    @Override
+    protected void onDestroy() {
+        preferences.edit().putBoolean(ConstantsOnlyForAndroid.PREF_IS_THROW_ACTIVITY_ACTIVE,
+                ConstantsOnlyForAndroid.PREF_IS_NOT_THROW_ACTIVITY_ACTIVE).apply();
+        super.onDestroy();
+    }
 }
