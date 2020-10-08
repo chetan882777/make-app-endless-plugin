43aebd33fe7fbdc9c5dbab058190f503eb4ea7bc
chetan pawar <chetan882777@gmail.com>
2020-10-08 09:00:44 +0530

    make constants class private

--- lib/make_app_endless_plugin.dart
+++ lib/make_app_endless_plugin.dart
@@ -1,20 +1,28 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:make_app_endless_plugin/constants.dart';
 
+// Method Channel for plugin
+const MethodChannel _channel = const MethodChannel(_Constants.METHOD_CHANNEL_ID);
+
+// Plugin class
 class MakeAppEndlessPlugin {
-  static const String TAG = "MzLifecycleExtensionPlugin";
-  static const MethodChannel _channel =
-      const MethodChannel(Constants.METHOD_CHANNEL_ID);
+
 
   Future<void> extendLifeCycle(String channelName, String channelDescription,
       String contentTitle, String contentText) async {
-    await _channel.invokeMethod(Constants.METHOD_CALL_START_SERVICE,
+    await _channel.invokeMethod(_Constants.METHOD_CALL_START_SERVICE,
         [channelName, channelDescription, contentTitle, contentText]);
   }
 
   Future<void> stopLifeCycleExtension() async {
-    await _channel.invokeMethod(Constants.METHOD_CALL_STOP_SERVICE);
+    await _channel.invokeMethod(_Constants.METHOD_CALL_STOP_SERVICE);
   }
+}
+
+class _Constants {
+  static const METHOD_CHANNEL_ID =
+      "com.chetan.make_app_endless_plugin.method_channel";
+  static const METHOD_CALL_START_SERVICE = "method_call_start_service";
+  static const METHOD_CALL_STOP_SERVICE = "method_call_stop_service";
 }
